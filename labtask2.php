b6ea7277bd9aaaabb12395364f71d34726380291
hasibhasan1839 <hasibhasan1839@gmail.com>
2025-07-23 13:47:15 +0600

    upload

---
+++ labtask2.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+ 
+<title>  customer  registration form</title>
+
+<body>
+ <center>
+    <h1 style ="color:blue;">Bank management System</h1>
+    <h2 style ="color:green;">your trusted financial partner</h2>
+ </center>
+ <h4 align="left">customer registration form</h4>
+ <table>
+    <tr> <td> Full Name: </td> <td> <input type="text"> </td> </tr>
+    <tr> <td> Date of Birth: </td> <td> <input type="date"> </td> </tr> 
+     
+    
+    <tr> <td> Gender  </td> <td> <input type = radio name ="des">Male</td>
+    <td> <input type = radio name ="des">Female</td>
+    <td> <input type = radio name ="des">Other</td>
+    
+
+
+</tr>
+   <td>Merital Status:</td>
+<td> 
+      <select name ="Married Status">
+        <option value ="">select</option> 
+        <option value ="single" >single</option> 
+        <option value ="married"> Married </option>
+      </select>
+</td>
+
+
+
+
+<tr>
+    <tr> 
+
+    <td>Account Type : </td>
+
+    <td>
+        <select name = "account type ">
+            <option value ="" >select<option>
+            <option value ="current">Current </option>
+            <option value =" Savings "> Savings </option>
+</select>
+</td>
+</tr>     
+    
+</tr>
+     <td> Mobile  Number : </td><td><input type= "number"></td>
+</tr>
+
+</tr>
+     <td> Email Address : </td><td><input type= "email"></td>
+</tr>
+
+<tr>
+    <td>address: </td>
+    <td><textarea rows= "4"cols ="50" </td>
+</tr>
+
+
+
+
+
+
+ </table>
+
+
+
+ </body>   
+
+
+<html>
